2daf8842390f3f1859e0df29b847346e5c3d55d2
Joao Pedro Simsic <joao.simsic@4x4brasil.com.br>
2026-01-29 17:05:02 -0300

    fix: removing old dtos

--- floyd/dtos/AIConfigDTO.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class AIConfigDTO(BaseModel):
-    diff_limit: int = Field(default=-1)
-    instructions: str = Field(default="")
--- floyd/dtos/PullRequestDTO.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class PullRequestDTO(BaseModel):
-    title: str
-    body: str
